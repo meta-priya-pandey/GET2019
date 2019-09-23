199f9a14b4ac2c2ff732982b908a1721298e4872
Priya Pandey <priya.pandey@metacube.com>
2019-09-23 17:19:32 +0530

    Assignment submitted

---
+++ EAD/Ead-Session11/src/main/java/com/metacube/Student/ServletInitializer.java
@@ -0,0 +1,16 @@
+/**
+ * Initializer Class
+ * @author Jyoti
+ */
+package com.metacube.Student;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StudentApplication.class);
+	}
+}
